33aa9a9d57583f608cffc278533d54cae3cc631f
Abdullah <abdallahmosbah25@gmail.com>
2024-02-25 21:04:09 +0200

    render base.html

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 views = Blueprint("views", __name__)
 
 @views.route("/home")
 def home_page():
-    return "Home Page"
+    return render_template('home.html')
 
 @views.route("/history")
 def histosy():
