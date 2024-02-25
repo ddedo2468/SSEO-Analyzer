e8b70b3bcb23a2d2ab318178cff65ba2951de4a5
Abdullah <abdallahmosbah25@gmail.com>
2024-02-25 21:55:09 +0200

    created a simple front-end base.html, login and signup

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route('/login')
 def login():
-    return "Login"
+    return render_template('login.html', title='login')
 
 
 @auth.route('/signup')
 def signup():
-    return "sign up"
-
+    return render_template('signup.html', title='signup')
 
 @auth.route('/logout')
 def logout():
