4c1c9fe83f06d88e64ff346c68274e00f4a6df01
Abdullah <abdallahmosbah25@gmail.com>
2024-02-27 20:04:48 +0200

    fix th flash messages

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
@@ -9,17 +9,25 @@ def home_page():
     return render_template('home.html')
 
 @views.route("/history")
+@login_required
 def histosy():
     return "Search History"
 
 @views.route("/analyzer")
+@login_required
 def analyzer():
-    return "analyzer Page"
+    
+    if request.method == 'POST':
+        url = request.form.get('url')
+
+    return render_template('seo_analyzer.html')
 
 @views.route("/counter")
+@login_required
 def counter():
     return "comming soon"
 
 @views.route("/crawler")
+@login_required
 def crawler():
     return "comming soon"
